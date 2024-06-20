227c747248db7b324d2a5c015ee83da0b2b9baae
Gayaththri <sriigaya3@gmail.com>
2024-06-20 15:18:12 +0530

    Added Coin details page with line chart for historical data using Google Charts

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -1,7 +1,77 @@
+import { useContext, useEffect, useState } from "react";
 import "./Coin.css";
+import { useParams } from "react-router-dom";
+import { CoinContext } from "../../contex/CoinContext";
+import LineChart from "../../components/LineChart/LineChart";
 
 const Coin = () => {
-  return <div>Coin</div>;
+  const { coinId } = useParams();
+  const [coinData, setCoinData] = useState();
+  const [historicalData, setHistoricalData] = useState();
+  const { currency } = useContext(CoinContext);
+
+  const fetchCoinData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-Fh7VgWtqTwFk8QgUD8xanjk1",
+      },
+    };
+
+    fetch(`https://api.coingecko.com/api/v3/coins/${coinId}`, options)
+      .then((response) => response.json())
+      .then((response) => setCoinData(response))
+      .catch((err) => console.error(err));
+  };
+
+  const fetchHistoricalData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-Fh7VgWtqTwFk8QgUD8xanjk1",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setHistoricalData(response))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchCoinData();
+    fetchHistoricalData();
+  }, [currency]);
+
+  if ((coinData, historicalData)) {
+    return (
+      <div className="coin">
+        <div className="coin-name">
+          <img src={coinData.image.large} alt="" />
+          <p>
+            {" "}
+            <b>
+              {coinData.name} ({coinData.symbol.toUpperCase})
+            </b>
+          </p>
+        </div>
+        <div className="coin-chart">
+          <LineChart historicalData={historicalData} />
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinner">
+        <div className="spin"></div>
+      </div>
+    );
+  }
 };
 
 export default Coin;
